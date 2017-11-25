88cf7a5ba5739e3b9e062de16cc9d5ff4ddc08b1
lcaresia <lucascarezia@gmail.com>
2017-11-25 21:08:25 -0200

    Primeiro commit

---
+++ EditorCode/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace EditorCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.codearea = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // codearea
+            // 
+            this.codearea.AcceptsReturn = true;
+            this.codearea.AcceptsTab = true;
+            this.codearea.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.codearea.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.codearea.ForeColor = System.Drawing.Color.White;
+            this.codearea.Location = new System.Drawing.Point(320, 12);
+            this.codearea.Multiline = true;
+            this.codearea.Name = "codearea";
+            this.codearea.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.codearea.Size = new System.Drawing.Size(798, 581);
+            this.codearea.TabIndex = 0;
+            this.codearea.WordWrap = false;
+            this.codearea.TextChanged += new System.EventHandler(this.codearea_TextChanged);
+            this.codearea.KeyDown += new System.Windows.Forms.KeyEventHandler(this.codearea_KeyDown);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1130, 605);
+            this.Controls.Add(this.codearea);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox codearea;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ EditorCode/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EditorCode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void codearea_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void codearea_KeyDown(object sender, KeyEventArgs e)
+        {
+            int selectionStart = codearea.SelectionStart;
+
+            if(e.KeyCode == Keys.OemCloseBrackets)
+            {
+                codearea.Text = codearea.Text.Insert(selectionStart, "]");
+                codearea.SelectionStart = selectionStart;
+                //codearea
+            }
+
+            if (e.Control)
+            {
+                if(e.KeyCode == Keys.Add)
+                {
+                    setFont(codearea, +2, true);
+                }
+                if (e.KeyCode == Keys.Subtract)
+                {
+                    setFont(codearea, -2, true);
+                }
+              
+            }
+            if (e.KeyCode == Keys.Tab)
+            {
+                int wordStartPosition = getPositionWordAt(codearea.Text, codearea.SelectionStart);
+
+                String word = codearea.Text.Substring(wordStartPosition, selectionStart - wordStartPosition);
+
+                if(word == "function")
+                {
+                    codearea.Text = codearea.Text.Remove(wordStartPosition, word.Length).Insert(wordStartPosition, "function() {" + Environment.NewLine + "    " + Environment.NewLine + "}");
+
+                    codearea.SelectionStart = selectionStart + 10;
+                }
+                e.SuppressKeyPress = true;
+                e.Handled = true;
+            }
+        }
+
+        public int getPositionWordAt(String text, int index, bool before = true)
+        {
+            if(index <= 0)
+            {
+                return index;
+            }
+            if(before)
+            {
+                int cont = index - 1;
+                bool searching = true;
+
+                while(searching)
+                {
+                    if(cont == 0 && text.Substring(cont, 1) != " ")
+                    {
+                        searching = false;
+                        return cont;
+                    }
+                    if(text.Substring(cont, 1) == " ") {
+                        searching = false;
+                        cont++;
+                        return cont;
+                    }
+                    else
+                    {
+                        cont --;
+                    }
+                }
+
+                return cont;
+            }
+
+            return index;
+        }
+
+        public void setFont(TextBox textbox, int size, bool current_font)
+        {
+            if(current_font)
+            {
+                textbox.Font = new Font(textbox.Font.FontFamily, textbox.Font.Size + size);
+            }
+            else
+            {
+                textbox.Font = new Font(textbox.Font.FontFamily, size);
+            }
+           
+        }
+    }
+}
